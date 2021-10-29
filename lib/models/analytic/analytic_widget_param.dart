b025f86155c61489b8879257b80397d4304fac92
Hanif Budiarto <m.hanifbudiarto@gmail.com>
2021-10-30 05:47:30 +0700

    add options to analyticWidgetParam

--- lib/models/analytic/analytic_widget_param.dart
+++ lib/models/analytic/analytic_widget_param.dart
@@ -3,14 +3,16 @@ part of sam_models_analytics;
 class AnalyticWidgetParam {
   final String title;
   final String model;
+  final ChartOptions? options;
 
-  AnalyticWidgetParam(
-      {required this.title, required this.model});
+  AnalyticWidgetParam({required this.title, required this.model, this.options});
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['title'] = this.title;
     data['model'] = this.model;
+    if (options != null) data['options'] = this.options;
+
     return data;
   }
 }
